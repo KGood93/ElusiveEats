f2b9f01cde9e850044e0ecc8dc78e986f3d27e6c
KA Goodreau <goodreau.kath@gmail.com>
2019-06-01 08:38:58 -0500

    added google maps API

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let totalNum;
 let numStart;
 let url;
 let apiKey = "7c817eeb531cb578b30c389378fbbabd"; 
+let map, infoWindow;
 
 function startScreen () {
     $('.startScreen').on('click', '.feedMe', function(event) {
@@ -99,6 +100,8 @@ function returnRestInfo(responseJson) {
     }
 
     $('.results').replaceWith(food);
+
+    //$('.map').removeClass('hidden');
    
     $('.info').removeClass('hidden');
 
@@ -119,6 +122,38 @@ function watchAgain() {
         numberGenerator();
     });
 }
+
+function initMap() {
+    map = new google.maps.Map(document.getElementsById('map'), {center: {lat: -34.397, lng: 150.644}, zoom: 6});
+    infoWindow = new google.maps.InfoWindow;
+
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(function(position) {
+            let pos = {
+                lat: position.coords.latitude, 
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);},
+            function() {
+                handleLocationError(true, infoWindow, map.getCenter());
+        });
+    }
+
+    else {
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ? 'Error: The Geolocation Service Failed.' : 'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+}
+
 
 function elusiveEats () {
     startScreen();
