37074fb5aab4877a3562fa6021242f9a278ffd2c
KA Goodreau <goodreau.kath@gmail.com>
2019-06-04 15:40:39 -0500

    display results 25

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ function callback(results, status) {
 function foodList() {
     console.log("1");
     let food = $('<ul class="results"></ul>');
-    for (let i = 0; i <= places.length; i++) {
+    for (let i = 0; i < places.length; i++) {
         console.log("2");
         console.log(places[i].name);
         food.append(`<li><h3>${places[i].name} - ${places[i].rating}</h3>
